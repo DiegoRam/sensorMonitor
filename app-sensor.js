2e5a491bbcbb3603697dddbc4af7bd683706c666
diegoram <ddarkheart@gmail.com>
2014-07-22 17:58:39 -0300

    add charts

--- app-sensor.js
+++ app-sensor.js
@@ -1,1 +1,22 @@
-hello SensorMonitor
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
+
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'jade')
+app.use('/public', express.static(__dirname + '/static'));
+
+app.get('/',function(req, res){
+	res.render('index');
+});
+
+io.on('connection',function(socket){
+	console.debug('a user has connected');
+});
+
+
+
+http.listen(3000,function(){
+	console.log("Starting server at port: 3000");
+});
